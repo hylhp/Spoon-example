77b5728270e8fcc724bfebdec82902929d2ec1fa
Haopeng <hylhp@sjtu.edu.cn>
2017-02-22 12:20:55 -0600

    init

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+  int a;
+  int b;
+  class Inner {
+    public int i;
+  }
+  public void print(boolean tt) throws Exception{
+    a = 0;
+    //asdasdsda
+    System.out.println("a: " + a);
+  }
+}
+
+class Test {
+  float q;
+}
